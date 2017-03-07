a8cd3d5601f240b1cec7226891d6586cce934c50
ArtyomResh <artyomresh@gmail.com>
2017-03-07 14:02:49 +0300

    refactor(social): integrating new function for social link for facebook

--- app/blocks/social/index.js
+++ app/blocks/social/index.js
@@ -1,27 +1,58 @@
 /* eslint-disable */
+// import $ from 'jquery';
+//
+// $(() => {
+// 	FB.init({
+// 		appId: 1805267866466228,
+// 		xfbml: true,
+// 		version: 'v2.5'
+// 	});
+//
+// 	$('.js-social-fb').click(event => {
+// 		event.preventDefault();
+// 		const $this = $(event.target);
+// 		const link = $this.attr('data-url');
+// 		const name = $this.attr('data-title');
+// 		const description = $this.attr('data-text');
+// 		const picture = $this.attr('data-image');
+// 		FB.ui({
+// 			method: 'feed',
+// 			link,
+// 			name,
+// 			description,
+// 			picture
+// 		}, () => {})
+// 	})
+// })
 import $ from 'jquery';
 
-$(() => {
-	FB.init({
-		appId: 1805267866466228,
-		xfbml: true,
-		version: 'v2.5'
-	});
+export function socialShare() {
+	const urlValue = document.origin + document.location.pathname;
 
-	$('.js-social-fb').click(event => {
-		event.preventDefault();
-		const $this = $(event.target);
-		const link = $this.attr('data-url');
-		const name = $this.attr('data-title');
-		const description = $this.attr('data-text');
-		const picture = $this.attr('data-image');
-		FB.ui({
-			method: 'feed',
-			link,
-			name,
-			description,
-			picture
-		}, () => {})
-	})
-})
+	function socialGenerator(url, title = '') {
+		const encUrl = encodeURIComponent(url ? url : urlValue);
+		const encTitle = encodeURIComponent(title);
+
+		return {
+			fb: `http://www.facebook.com/sharer.php?u=${encUrl}`,
+			tw: `http://twitter.com/share?url=${encUrl}&text=${encTitle}`
+		};
+	}
+
+	$('.js-share').each(function () {
+		const $shareSelector = $(this);
+		// const shareTitle = $shareSelector.data('share-title'); для указания кастомных тайтлов
+		const shareText = $shareSelector.data('share-text');
+		// const shareUrl = $shareSelector.data('share-url');// если понадобится указывать url вручную, подставить как параметр в socialGenerator
+		// Закомментил, потому что пока не используем эти переменнные, EsLint ругается
+		if ($shareSelector.hasClass('facebook')) {
+			$shareSelector.prop('href', socialGenerator().fb);
+		}
+
+		else if ($shareSelector.hasClass('twitter')) {
+			$shareSelector.prop('href', socialGenerator(null, shareText).tw);
+		}
+	});
+}
+
 /* eslint-enable */
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,10 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
-import '../blocks/social';
+// import '../blocks/social';
+import {socialShare}
+	from '../blocks/social';
 
 $(() => {
 	svg4everybody();
+	socialShare();
 });
